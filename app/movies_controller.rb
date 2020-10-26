558b12e06f7b8b01eff63247c06abe6d1c28d0f7
Jung Lee <lee.jung.s95@gmail.com>
2020-10-26 00:45:09 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -71,7 +71,7 @@ def can_be_found_updated_and_saved
   # Updtate the title "Awesome Flick" to "Even Awesomer Flick", save it, then return it
   movie = Movie.create(title: "Awesome Flick")
   movie = Movie.find_by(title: "Awesome Flick")
-  movie.save
+  movie.update(title: "Even Awesomer Flick")
   movie.title
   __
 end
